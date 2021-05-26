3082bb6210273b851b268bb0178f881c7bc55f8d
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-05-26 21:16:36 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,21 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../api/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 import useVideos from "../hooks/useVideos";
 
 const App = () => {
-    const [videos, setVideos] = useVideos("Metal");
+    const [videos, search] = useVideos("Metal");
     const [selectedVideo, setSelectedVideo] = useState(null);
 
-    useEffect(() => onFormSubmit("Metal"));
-
     useEffect(() => {
-        setVideos(videos[0]);
-    }, [videos]);
+        setSelectedVideo(videos[0]);
+    }, [selectedVideo]);
 
-    const onFormSubmit = async (value) => {
-        await youtube.get("/search", { params: { q: value } })
-            .then(r => initData(r))
-            .catch(e => console.error(e));
-    }
-
-    const onSelectedVideo = (video) => {
-        setSelectedVideo(video);
-    }
 
     return (
         <div className='ui container'>
-            <SearchBar onSubmit={onFormSubmit}/>
+            <SearchBar onSubmit={search}/>
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
@@ -36,7 +24,7 @@ const App = () => {
                     <div className="five wide column">
                     <VideoList
                         videos={videos}
-                        onVideoSelect={onSelectedVideo}
+                        onVideoSelect={setSelectedVideo}
                     />
                     </div>
                 </div>
